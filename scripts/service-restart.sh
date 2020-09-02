c4d1048cfe9c35d397e0152a1e367c641c860c66
Andrei <at@senti.dk>
2020-09-02 20:37:51 +0200

    Dev

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/tagmanager/production
 	systemctl restart senti-tag-manager.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Gateway MASTER updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Tag Manager MASTER updated and restarted!"}' $2
 	echo
 	exit 0
 fi
@@ -14,7 +14,7 @@ if [[ "$1" == "dev" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/tagmanager/development
 	systemctl restart senti-tag-manager-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Gateway DEV updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Tag Manager DEV updated and restarted!"}' $2
 	echo
 	exit 0
 fi
