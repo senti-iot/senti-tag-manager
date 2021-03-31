5882198b725281199c5ec9527fa5ea5312a85e7b
Andrei <at@senti.dk>
2021-03-31 20:06:31 +0200

    Slack Blocks

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -5,7 +5,36 @@ if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/tagmanager/production
 	systemctl restart senti-tag-manager.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Tag Manager MASTER updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Senti Tag Manager updated",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Branch:*\n'$1'"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Travis",
+					"emoji": true
+				},
+				"value": "travis-link",
+				"url": "http://travis-ci.com/github/senti-iot/senti-tag-manager",
+				"action_id": "button-action"
+			}
+		}
+	]
+}' $2
 	echo
 	exit 0
 fi
@@ -14,7 +43,36 @@ if [[ "$1" == "dev" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/tagmanager/development
 	systemctl restart senti-tag-manager-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Tag Manager DEV updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Senti Tag Manager updated",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Branch:*\n'$1'"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Travis",
+					"emoji": true
+				},
+				"value": "travis-link",
+				"url": "http://travis-ci.com/github/senti-iot/senti-tag-manager",
+				"action_id": "button-action"
+			}
+		}
+	]
+}' $2
 	echo
 	exit 0
 fi
